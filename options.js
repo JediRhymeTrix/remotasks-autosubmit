ccbe57302fdceebd1da60b4a1697e421eeabbaca
JediRhymeTrix <ved914980@gmail.com>
2023-11-03 16:04:20 -0500

    feat(options): add hours and minutes input fields to options page
    - This commit includes changes to options.html and options.js to add the new options for setting the time remaining.

--- options.js
+++ options.js
@@ -1,9 +1,19 @@
 // When the options page is loaded, get the current options and update the UI
-chrome.storage.sync.get("playSound", function (data) {
+chrome.storage.sync.get(["playSound", "hours", "minutes"], function (data) {
     document.getElementById("playSound").checked = data.playSound;
+    document.getElementById("hours").value = data.hours;
+    document.getElementById("minutes").value = data.minutes;
 });
 
-// When the checkbox is clicked, save the new option
+// When the checkboxes are clicked, save the new options
 document.getElementById("playSound").addEventListener("change", function (e) {
     chrome.storage.sync.set({ playSound: e.target.checked });
+});
+
+document.getElementById("hours").addEventListener("change", function (e) {
+    chrome.storage.sync.set({ hours: e.target.value });
+});
+
+document.getElementById("minutes").addEventListener("change", function (e) {
+    chrome.storage.sync.set({ minutes: e.target.value });
 });

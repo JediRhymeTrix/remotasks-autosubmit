a03808e683977bc0162d45454466d0da4f582726
JediRhymeTrix <ved914980@gmail.com>
2023-11-04 18:27:25 -0500

    fix(options): correct remaining time type error
    Changes: Fixed the type error in options.js where the remaining time was not correctly converted to minutes.

--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 // When the options page is loaded, get the current options and update the UI
 chrome.storage.sync.get(["playSound", "hours", "minutes"], function (data) {
     document.getElementById("playSound").checked = data.playSound;
-    document.getElementById("hours").value = data.hours;
-    document.getElementById("minutes").value = data.minutes;
+    document.getElementById("hours").value = data.hours.toString();
+    document.getElementById("minutes").value = data.minutes.toString();
 });
 
 // When the checkboxes are clicked, save the new options
@@ -11,9 +11,9 @@ document.getElementById("playSound").addEventListener("change", function (e) {
 });
 
 document.getElementById("hours").addEventListener("change", function (e) {
-    chrome.storage.sync.set({ hours: e.target.value });
+    chrome.storage.sync.set({ hours: parseInt(e.target.value, 10) });
 });
 
 document.getElementById("minutes").addEventListener("change", function (e) {
-    chrome.storage.sync.set({ minutes: e.target.value });
+    chrome.storage.sync.set({ minutes: parseInt(e.target.value, 10) });
 });

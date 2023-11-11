f03d757686b3cee0e3ecfdc7dfdd162bb5951aaf
JediRhymeTrix <ved914980@gmail.com>
2023-11-10 21:50:27 -0600

    feat(options): add reset button to revert settings to default values

--- options.js
+++ options.js
@@ -1,3 +1,10 @@
+// Default values
+const DEFAULT_OPTIONS = {
+    playSound: true,
+    hours: 0,
+    minutes: 0,
+};
+
 // When the options page is loaded, get the current options and update the UI
 chrome.storage.sync.get(["playSound", "hours", "minutes"], function (data) {
     document.getElementById("playSound").checked = data.playSound;
@@ -16,4 +23,17 @@ document.getElementById("hours").addEventListener("change", function (e) {
 
 document.getElementById("minutes").addEventListener("change", function (e) {
     chrome.storage.sync.set({ minutes: parseInt(e.target.value, 10) });
+});
+
+// When the reset button is clicked, reset the options to default
+document.getElementById("reset").addEventListener("click", function () {
+    chrome.storage.sync.set(DEFAULT_OPTIONS, function () {
+        // Update the UI to reflect the default options
+        document.getElementById("playSound").checked =
+            DEFAULT_OPTIONS.playSound;
+        document.getElementById("hours").value =
+            DEFAULT_OPTIONS.hours.toString();
+        document.getElementById("minutes").value =
+            DEFAULT_OPTIONS.minutes.toString();
+    });
 });
